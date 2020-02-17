02fb969f02b7f9ebd9f3ca7dc0faa1859646315b
Michael Lehotay <mlehotay@gmail.com>
2020-02-17 17:12:33 -0500

    add submit button and results page to web app

--- broccoli/app/main.py
+++ broccoli/app/main.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import pandas as pd
+import pickle
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
+#pipe = pickle.load(open('model/pipe.pkl', 'rb'))
 
 @app.route("/")
 def index():
-    return 'Eat more broccoli!'
+    return render_template('index.html')
+
+@app.route('/result', methods=['POST'])
+def result():
+    args = request.form
+    return render_template('result.html')
 
 if __name__ == '__main__':
     app.run()

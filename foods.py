086492893c38d8698c8c0299ff1ac3963c378105
Michael Lehotay <mlehotay@gmail.com>
2020-02-21 06:30:56 -0500

    add foods to input page

--- foods.py
+++ foods.py
@@ -1,7 +1,29 @@
+import random
 import pandas as pd
 
 def food_grid():
-    return '<tr><td>16 foods go here</td></tr>'
+    s = '\n'
+    df = pd.read_csv('data/foods.csv')
+    foods = random.sample(df['Food'].to_list(), k=16)
+    i = 0
+    for food in foods:
+        slug = food.replace(' ', '')
+        filename = f'static/images/{slug}.jpg'
+        if (i%4) == 0:
+            s += '<tr>\n'
+        s += '<td align="center">'
+        s += f'{food}<br><img src="{filename}" height="50%" width="50%" alt="picture of {food}"><br>\n'
+        s += '<small>'
+        s += f'<input type="radio" id="{slug}-like" name="{slug}" value="1"><label for="{slug}-like">like</label>\n'
+        s += f'<input type="radio" id="{slug}-neutral" name="{slug}" value="0" checked><label for="{slug}-neutral">neutral</label>\n'
+        s += f'<input type="radio" id="{slug}-dislike" name="{slug}" value="-1"><label for="{slug}-dislike">dislike</label>\n'
+        s += '</small>'
+        s += '</td>'
+        if (i%4) == 3:
+            s += '\n</tr>'
+        s += '\n'
+        i += 1
+    return s
 
 def food_survey():
     s = ''
